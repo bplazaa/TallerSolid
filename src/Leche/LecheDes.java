ecf85da5125b684310c37adbd103aa8a66cc996e
Karla Duran Oscuez <karlitadur@hotmail.com>
2020-06-25 21:42:49 -0500

    clase LecheDes.java agregada

---
+++ src/Leche/LecheDes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Leche;
+
+/**
+ *
+ * @author Bryan Plaza
+ */
+public abstract class LecheDes extends LecheEntera{
+    public abstract void usarPastel();
+}
